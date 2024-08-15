627185f973b6018be03b5edb9fc8281c24fc07c3
Vikas Thakur <vikas.t@dgsl.in>
2024-08-15 12:18:22 +0530

    Initial commit

---
+++ src/ex_08082024/Lab002.py
@@ -0,0 +1,10 @@
+print(2+2)
+list1 = [10, 20, 4, 45, 99]
+print("Largest element is:", max(list1))
+print("Largest element is:", min(list1))
+# sorting the list
+list1.sort()
+print(list1)
+print("Largest element is:", list1[-1])
+list1.sort(reverse=True)
+print(list1)
---
+++ src/ex_08082024/Lab003.py
@@ -0,0 +1,6 @@
+print("Amit","Rahul","Ajay")
+
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+num=num1+num2
+print(num)
