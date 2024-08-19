741136abfde0f44e5fd2a115fb2820dbcee8d8c7
vikibond11 <vikas15092012@gmail.com>
2024-08-19 16:46:36 +0530

    added Lab004

--- src/ex_08082024/Lab004.py
+++ src/ex_08082024/Lab004.py
@@ -1,1 +1,7 @@
+import math
+
 print("one","two","three",sep=':',end="_")
+r=float(input("Enter the radius="))
+area = math.pi*(r**2)
+print("Area of circle:",area)
+print(f"Area of circle:{area:.2f}")
